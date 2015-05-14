0cd2e2244b4c75ebfcf204fa026ffbebcfe7bdbf
Araceli <araceliteruel@gmail.com>
2015-05-14 12:24:49 +0200

    Implementacion TestAlumno

--- unit9_b/src/unit9_b/Unit9_b.java
+++ unit9_b/src/unit9_b/Unit9_b.java
@@ -15,7 +15,6 @@ public class Unit9_b {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
---
+++ unit9_b/test/unit9_b/AlumnoTest.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unit9_b;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Araceli Teruel
+ */
+public class AlumnoTest {
+    
+    public AlumnoTest() {
+    }
+
+    /**
+     * Test of validaNif method, of class Alumno.
+     */
+    @Test(expected=NullPointerException.class)
+    public void testValidaNif() {
+        Alumno al=new Alumno();
+        boolean expected, actual;
+        expected=false;
+        actual=al.validaNif("4866");
+        assertEquals(expected, actual);
+        
+        expected=false;
+        actual=al.validaNif(null);
+        assertEquals(expected, actual);
+        
+        expected=false;
+        actual=al.validaNif("a48667898");
+        assertEquals(expected, actual);
+        
+        expected=false;
+        actual=al.validaNif("48667898I");
+        assertEquals(expected, actual);
+        
+        expected=true;
+        actual=al.validaNif("48667898J");
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test of calculaTasaMatricula method, of class Alumno.
+     */
+    @Test
+    public void testCalculaTasaMatricula() {
+        Alumno al=new Alumno();
+        float expected, actual;
+        actual=al.calculaTasaMatricula(12, false, true);
+        expected=2000.00f;
+        assertEquals(expected, actual, 0);
+        
+        actual=al.calculaTasaMatricula(15, true, true);
+        expected=250.00f;
+        assertEquals(expected, actual, 0);
+        
+        actual=al.calculaTasaMatricula(15, false, false);
+        expected=500.00f;
+        assertEquals(expected, actual, 0);
+        
+        actual=al.calculaTasaMatricula(70, false, true);
+        expected=250.00f;
+        assertEquals(expected, actual, 0);
+        
+        actual=al.calculaTasaMatricula(55, true, true);
+        expected=150.00f;
+        assertEquals(expected, actual, 0);
+    }
+    
+}
